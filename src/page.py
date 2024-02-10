82f20a89960addae3cd0c0b2718e80659aa7ee50
arjunhm <hm.arjun97@gmail.com>
2024-02-09 23:16:04 -0500

    Added last_accessed decorator for Page

--- src/page.py
+++ src/page.py
@@ -1,3 +1,5 @@
+import time
+
 PAGE_SIZE = 5
 
 
@@ -14,10 +16,23 @@ class Page:
         self.data = CustomByteArray(PAGE_SIZE)
         self.ref_count = 0
         self.is_dirty = False
+        self.last_accessed = 0
 
+    def last_accessed_decorator(func):
+        def wrapper(self):
+            self.last_accessed = time.time()
+            func(self)
+
+        return wrapper
+
+    @last_accessed_decorator
     def incr_ref_count(self):
         self.ref_count += 1
 
+    @last_accessed_decorator
     def decr_ref_count(self):
         if self.ref_count > 0:
             self.ref_count -= 1
+
+    def set_last_accessed(self):
+        self.last_accessed = time.time()
