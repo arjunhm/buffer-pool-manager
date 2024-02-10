944b35effddf748684995fe628c1dcf2a32cd9d0
arjunhm <hm.arjun97@gmail.com>
2024-02-09 23:17:43 -0500

    Updated DiskManager - write_page now sets is_dirty to False

--- src/disk_manager.py
+++ src/disk_manager.py
@@ -6,21 +6,20 @@ from src.page import Page
 
 MAX_NUM_PAGES = 15
 
-class DiskManager:
 
+class DiskManager:
     def __init__(self):
-        self.num_pages = -1 # Tracks number of pages
-        self.pages = dict() # pages[page_id] = Page
+        self.num_pages = -1  # Tracks number of pages
+        self.pages = dict()  # pages[page_id] = Page
 
     def read_page(self, page_id: int) -> Page:
         return self.pages[page_id]
 
-
     def write_page(self, page: Page):
         self.pages[page.id] = page
+        self.page.is_dirty = False
 
     def allocate_page(self, page_id: int) -> int:
-
         if self.num_pages >= MAX_NUM_PAGES:
             raise ValueError("Page limit exceeded")
 
@@ -30,5 +29,3 @@ class DiskManager:
     # FIXME decrement num_pages and fix how pageID is assigned. Too sleepy rn
     def deallocate_page(self, page_id: int):
         del self.pages[page_id]
-
-
